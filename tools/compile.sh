bbfb32ab318a8a6f16423eb667dcbb52079e3be4
lindeer <le.chang118@gmail.com>
2025-07-29 16:14:08 +0800

    build in docker

---
+++ tools/compile.sh
@@ -0,0 +1,13 @@
+echo '--- Inside ARM container ---'
+
+echo 'Verifying glibc version inside container...'
+ldd --version || true
+dpkg -s libc6 | grep Version || true
+
+echo 'Installing build tools (g++, make, cmake) inside container...'
+apt-get update && apt-get install -y build-essential cmake
+
+cd lib-src/build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+cmake --build . --config Release --target Dictionaries
+cd -
