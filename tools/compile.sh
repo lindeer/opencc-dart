f082e4caef02e9ac8aa652c642fe9f1a2472b6f4
lindeer <le.chang118@gmail.com>
2025-07-29 23:24:06 +0800

    correct cmake

--- tools/compile.sh
+++ tools/compile.sh
@@ -5,12 +5,10 @@ ldd --version || true
 dpkg -s libc6 | grep Version || true
 
 echo 'Installing build tools (g++, make, cmake) inside container...'
-apt-get update && apt-get install -y build-essential cmake python
+apt-get update && apt-get install -y build-essential python wget
+wget https://github.com/Kitware/CMake/releases/download/v3.26.4/cmake-3.26.4-linux-x86_64.tar.gz
+tar xvf cmake-3.26.4-linux-aarch64.tar.gz -C /usr --strip-components=1
 
 cd lib-src/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-echo '===='
-ls deps
-echo '===='
 cmake --build . --config Release --target Dictionaries
-cd -
