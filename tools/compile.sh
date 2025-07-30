389371cc04e061d50e7c464bcd2dd359a72d3e54
lindeer <le.chang118@gmail.com>
2025-07-30 21:40:38 +0800

    refine bahs

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,9 +1,9 @@
 ARCH=$(uname -m)
-echo "--- Inside $ARCH container ---"
+echo "AAAAAA Inside $ARCH container AAAAAA"
 ldd --version || true
 dpkg -s libc6 | grep Version || true
 
-echo 'Installing build tools (g++, make, cmake) inside container...'
+echo "Installing build tools (g++, make, cmake) inside $ARCH container..."
 apt-get update && apt-get install -y build-essential python wget
 wget https://github.com/Kitware/CMake/releases/download/v3.26.4/cmake-3.26.4-linux-$ARCH.tar.gz
 tar xvf cmake-3.26.4-linux-$ARCH.tar.gz -C /usr --strip-components=1
