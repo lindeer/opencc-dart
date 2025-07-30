d5357de464ee0cb2f31f5e837d386d4c47edba5b
lindeer <le.chang118@gmail.com>
2025-07-30 21:07:34 +0800

    arch env var

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,13 +1,11 @@
-echo '--- Inside ARM container ---'
-
-echo 'Verifying glibc version inside container...'
+uname -ar
 ldd --version || true
 dpkg -s libc6 | grep Version || true
 
 echo 'Installing build tools (g++, make, cmake) inside container...'
 apt-get update && apt-get install -y build-essential python wget
-wget https://github.com/Kitware/CMake/releases/download/v3.26.4/cmake-3.26.4-linux-aarch64.tar.gz
-tar xvf cmake-3.26.4-linux-aarch64.tar.gz -C /usr --strip-components=1
+wget https://github.com/Kitware/CMake/releases/download/v3.26.4/cmake-3.26.4-linux-$ARCH.tar.gz
+tar xvf cmake-3.26.4-linux-$ARCH.tar.gz -C /usr --strip-components=1
 
 cd lib-src/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
